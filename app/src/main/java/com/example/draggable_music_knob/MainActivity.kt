a6eeb77677fb5c046423d00a03ec81a18277db73
gedeastu <gede.astu@gmail.com>
2024-02-04 23:00:25 +0800

    draggable music knob is done, but i need to more learn of this case

--- app/src/main/java/com/example/draggable_music_knob/MainActivity.kt
+++ app/src/main/java/com/example/draggable_music_knob/MainActivity.kt
@@ -1,30 +1,46 @@
 package com.example.draggable_music_knob
 
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.layout.boundsInWindow
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlin.math.PI
 import kotlin.math.atan2
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,19 +54,59 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DraggableMusicKnob(){
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Row {
-            Draggable()
+        Row(horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .border(
+                    1.dp, color = Color.Green, RoundedCornerShape(10.dp)
+                )
+                .padding(30.dp)) {
+            var volume by remember {
+                mutableFloatStateOf(0f)
+            }
+            val barCount = 20
+
+            Draggable(modifier = Modifier.size(100.dp)){
+                volume = it
+            }
             Spacer(modifier = Modifier.width(20.dp))
-            VolumeBar()
+            VolumeBar(
+                modifier = Modifier.fillMaxWidth().height(30.dp),
+                activeBars = (barCount * volume).roundToInt(),
+                barCount = barCount
+            )
         }
     }
 }
 
 @Composable
-fun VolumeBar(){
-    Text(text = "100%")
+fun VolumeBar(modifier: Modifier = Modifier,activeBars: Int = 0,barCount: Int = 10){
+    BoxWithConstraints(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+        ){
+        val barWidth = remember {
+            constraints.maxWidth / (2f * barCount)
+        }
+        Canvas(modifier = modifier, onDraw = {
+            for (i in 0 until barCount){
+                drawRoundRect(
+                    color =
+                    if (i in 0..activeBars){
+                            Color.Green
+                    }else{
+                            Color.DarkGray
+                    },
+                    topLeft = Offset(i * barWidth * 2f + barWidth / 2f,0f),
+                    size = Size(barWidth,constraints.maxHeight.toFloat()),
+                    cornerRadius = CornerRadius(0f)
+                )
+            }
+        })
+    }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun Draggable(
     modifier:Modifier = Modifier,
@@ -82,13 +138,37 @@ fun Draggable(
                 val windowBounds = it.boundsInWindow()
                 centerX = windowBounds.size.width / 2f
                 centerY = windowBounds.size.height / 2f
-
             }
-            .pointerInteropFilter {event ->
+            .pointerInteropFilter { event ->
                 touchX = event.x
                 touchY = event.y
-                val angle = -atan2(centerX - touchX, centerY - touchY)
+                val angle = -atan2(centerX - touchX, centerY - touchY) * (180f / PI).toFloat()
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN,
+                    MotionEvent.ACTION_MOVE
+                    -> {
+                        if (angle !in -limitingAngle..limitingAngle) {
+                            val fixedAngle = if (angle in -180f..-limitingAngle) {
+                                360f + angle
+                            } else {
+                                angle
+                            }
+                            rotation = fixedAngle
+
+                            val percent = (fixedAngle - limitingAngle) / (360f - 2 * limitingAngle)
+                            onValueChange(percent)
+                            true
+                        } else {
+                            false
+                        }
+                    }
+
+                    else -> {
+                        false
+                    }
+                }
             }
+            .rotate(rotation)
     )
 }
 
